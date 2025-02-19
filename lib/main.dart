f7ba2f311b296aeb6bfa99b805f16c0b5e927147
urstrulydeva <urstrulydeva1@gmail.com>
2025-02-19 12:45:25 +0530

    "Styling options explored"

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,23 @@
 import 'package:flutter/material.dart';
 
-void main(){
-  runApp(MaterialApp(home: Text('Hello I am Deva'),));
+void main() {
+  runApp(
+     MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [Color.fromARGB(255, 44, 7, 81),Color.fromARGB(255, 26, 1, 29)],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight
+            ),
+          ),
+          child: Center(
+            child:  const Text('Hello I am Deva', style: TextStyle(color: Colors.white,fontSize: 28),),
+            
+          ),
+        ),
+      ),
+    ),
+  );
 }
